43614acc77ef099a76ec8a0863bf02ae5dc6d670
rbaryla <robert@em4u.pl>
2017-01-08 02:03:14 +0100

    Dodane:  - express  - babel

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,23 @@
+/**
+ * Created by rbaryla on 08.01.2017.
+ */
+
+import express   from 'express';
+import path      from 'path';
+import open      from 'open';
+
+const port      = 3000;
+const app       = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+});
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,7 @@
+/**
+ * Created by rbaryla on 08.01.2017.
+ */
+
+import chalk from 'chalk';
+
+console.log(chalk.green('Starting app in dev mode...'));
